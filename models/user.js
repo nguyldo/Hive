163c05046767a01b941f990e3bc2a57ff484710e
nguyldo <nguyldo@gmail.com>
2020-05-05 23:05:58 -0700

    connected to database, added several libraries

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = User = mongoose.model("users", UserSchema)
--- server.js
+++ server.js
@@ -1,11 +1,28 @@
+// require node libraries
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv').config()
+
+// const variables
+const PORT = 3005;
+const DB_CONNECTION = process.env.DB_CONNECTION;
 
 const app = express();
 
-app.get('/testapi', (request, response) => {
-    response.json({ template: "Hello world!" });
+// set up body-parser
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+mongoose.connect(DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("MongoDB: successfully connected!"))
+    .catch(err => console.log(err));
+
+/* API endpoints */
+// /helloworld - returns the DB connection URI
+app.get('/helloworld', (request, response) => {
+    response.json({ template: DB_CONNECTION });
 });
 
-const PORT = 3005;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
